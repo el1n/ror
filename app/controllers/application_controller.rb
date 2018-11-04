f4b1d9ed533d6e4ce9c0eb2f0b666f3f4f0c425f
el1n <elin@mikomoe.jp>
2018-11-04 10:54:34 +0000

    -

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
+  def hello2
+    render html: "hello, world! (2)"
+  end
+end
